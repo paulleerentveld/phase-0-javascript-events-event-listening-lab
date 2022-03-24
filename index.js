88df9192edaa82b23955c4ea6ba5ff18b7268d55
paulleerentveld <paul@veldnet.com.au>
2022-03-24 13:11:07 +1000

    add JS for lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    input.addEventListener('click',onClickAlert);
 }
+
+function onClickAlert() {
+    alert('I was clicked!');
+}
+
+const input = document.getElementById('input');
+
+input.addEventListener('click',onClickAlert);
